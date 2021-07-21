db15cbfae7bec4586a8efb2442cdc2655c482238
iegpeppino <iegpeppino@outlook.com.ar>
2021-07-21 20:33:37 -0300

    Added uathentication

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,16 +10,17 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-    path(route='about_us/', view=views.about , name='about_us'),
+    path(route = 'about_us/', view = views.about , name = 'about_us'),
     # path for contact us view
-    path(route='contact_us/', view=views.contact , name='contact_us'),
+    path(route = 'contact_us/', view = views.contact , name = 'contact_us'),
     # path for registration
-
+    path(route = 'registration/', view = views.registration_request , name = 'registration'),
     # path for login
-
+    path(route = 'login/', view = views.login_request , name = 'login'),
     # path for logout
-
-    path(route='', view=views.get_dealerships, name='index'),
+    path(route = 'logout/', view = views.logout_request , name = 'logout'),
+    # path for home route
+    path(route='', view = views.get_dealerships, name='index'),
 
     # path for dealer reviews view
 
