c991b942bb3b8753eef0c03b86880ebf408fecd7
iegpeppino <iegpeppino@outlook.com.ar>
2021-07-25 21:26:31 -0300

    Updated missing admin models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
+from .models import  CarMake, CarModel
 # from .models import related models
 
 
 # Register your models here.
 
 # CarModelInline class
-
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 3
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    fields = ('name', 'description', 'country')
 
 # Register models here
+admin.site.register(CarModel)
+admin.site.register(CarMake, CarMakeAdmin)
