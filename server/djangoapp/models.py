8337026574554b599f79611dc61e23518344e080
iegpeppino <iegpeppino@outlook.com.ar>
2021-07-28 17:00:03 -0300

    general changes to templates and debug views

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -23,7 +23,7 @@ class CarMake(models.Model):
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
 # - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
+# - Type (CharField with a choices argument to proitide limited choices such as Sedan, SUV, WAGON, etc.)
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
